97afc22eca68e4e372765153b5d7b0764c640f79
FractalPomidor <89431779+FractalPomidor@users.noreply.github.com>
2023-01-31 21:16:30 -0500

    Set level information here

--- COEN244AdventureGameV1/Level.h
+++ COEN244AdventureGameV1/Level.h
@@ -1,15 +1,29 @@
 #pragma once
 #include <fstream>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#inlcude <map>
+
+using namespace std;
+
+const int MAX_ROW = 100; // sets the max row/ col to 100 but the code checks for empty spaces from txt file
+const int MAX_COL = 100; // and removes the empty spaces
+
 class Level
 {
-public:
-	Level(std::ifstream &levelFile);
-	void draw();
+  private:
+	char gameMap[MAX_ROW][MAX_COL];
+	int ROW, COL;
+	int playerRow, playerCol;
+  public:
+	Level();
+	Level(const string &fileName);
 
+  	void readMapFromFile(const string &fileName);
+	void displayMap();
+	void movePlayer(char direction);
 
-private:
-	char* map;
-	int maxLine = 0;
-	int lineCount;
 };
 
